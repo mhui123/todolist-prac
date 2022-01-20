7f978e20e58b200855d341eb90d933d862de22a7
mhui123 <mhui7864@gmail.com>
2022-01-20 16:12:52 +0900

    list와 모두삭제버튼 추가

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,13 +1,68 @@
 <template>
-  <div>list</div>
+  <section>
+      <ul>
+        <li v-for="(todoItem, idx) in todoItems" v-bind:key="idx">
+            <i class="checkBtn fas fa-check" aria-hidden="true"></i>
+            {{ todoItem }}
+            <span class="removeBtn" type="button" @click="removeTodo(todoItem, idx)">
+                <i class="far fa-trash-alt" aria-hidden="true"></i>
+            </span>
+
+        </li>
+      </ul>
+  </section>
 </template>
 
 <script>
 export default {
-
+    data() {
+        return {
+            todoItems: []
+        }
+    },
+    created(){
+        if(localStorage.length > 0){
+            for (let i = 0; i < localStorage.length; i ++){
+                this.todoItems.push(localStorage.key(i));
+            }
+        }
+    },
+    methods : {
+        removeTodo(todoItem, index) {
+            localStorage.removeItem(todoItem);
+            this.todoItems.splice(index, 1); // splice(startIdx, count): 배열에서 인덱스 시작지점부터 count만큼 삭제
+        }
+    }
 }
 </script>
 
-<style>
+<style scoped>
+    ul {
+        list-style-type: none;
+        padding-left: 0px;
+        margin-top: 0;
+        text-align: left;
+    }
 
+    li {
+        display: flex;
+        min-height: 50px;
+        height: 50px;
+        line-height: 50px;
+        margin: 0.5rem 0;
+        padding: 0 0.9rem;
+        background: white;
+        border-radius: 5px;
+    }
+
+    .checkBtn {
+        line-height: 45px;
+        color: #62acde;
+        margin-right: 5px;
+    }
+
+    .removeBtn {
+        margin-left: auto;
+        color: #de4343;
+    }
 </style>
